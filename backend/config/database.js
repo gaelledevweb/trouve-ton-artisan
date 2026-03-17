13838ec4d720c2ed9ed94d78e76c074894332963
GaelleDevWeb72 <gaelle.devweb72@gmail.com>
2026-03-17 17:36:34 +0100

    render

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,10 +7,17 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT || 22338, // Ajout du port Aiven
+        dialect: 'mysql',
         logging: false,
         define: {
             timestamps: false
+        },
+        // AJOUT INDISPENSABLE POUR AIVEN :
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
         }
     }
 );
