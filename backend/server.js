9aad2a72ebe0b0b84cf6ef86e32bdd8bdcb2dbf1
GaelleDevWeb72 <gaelle.devweb72@gmail.com>
2026-03-17 18:07:03 +0100

    render

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
+const sequelize = require('./config/database');
 const categoryRoutes = require('./routes/categoryRoutes');
 const artisanRoutes = require('./routes/artisanRoutes');
 
